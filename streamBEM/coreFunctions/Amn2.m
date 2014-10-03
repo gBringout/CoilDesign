6e4874dbc5186de36b040479c334b5124f1925c7
Gael Bringout <gael.bringout@gmail.com>
2014-10-03 18:06:26 +0200

    Update the way I handle parallel computing

--- streamBEM/coreFunctions/Amn2.m
+++ streamBEM/coreFunctions/Amn2.m
@@ -10,7 +10,7 @@ function [AX,AY,AZ] = Amn2(node_1, triangle_1, basis_1,node_2, triangle_2, basis
 
 
 
-[~,~,ck] = TriGaussPoints(2);
+[~,~,ck] = triGaussPoints(2);
 
 %%
 mu_0 = 4*pi*10^-7;
@@ -21,43 +21,46 @@ AX = zeros(size(node_1,2),size(node_2,2));
 AY = zeros(size(node_1,2),size(node_2,2));
 AZ = zeros(size(node_1,2),size(node_2,2));
 
-if matlabpool('size') == 0 % checking to see if my pool is already open
-    matlabpool open 4
+[TF,~] = license('checkout', 'Distrib_Computing_Toolbox');
+if matlabpool('size') == 0  && TF
+    % checking to see if the pool is already open and of we have the licence
+    matlabpool open
 end
 
 tic
 
-parfor n=1:size(node_1,2); %For every node on the induced surface
+parfor m=1:size(node_1,2); %For every node on the induced surface
     tempX = zeros(1,size(node_2,2));
     tempY = zeros(1,size(node_2,2));
     tempZ = zeros(1,size(node_2,2));
+    r = node_1(m).coord(:);
 
-    for m=1:size(node_2,2); %and every node on the coil surface
+    for n=1:size(node_2,2); %and every node on the coil surface
         superBigSumX = 0;
         superBigSumY = 0;
         superBigSumZ = 0;
         
-        for i=1:node_2(m).nbrTriangle; %Number of face in this face
-            currentTriangle_i = node_2(m).linkToTriangle(i);
+        for i=1:node_2(n).nbrTriangle; %Number of face in this face
+            currentTriangle_i = node_2(n).linkToTriangle(i);
             integral = 0;
-            vmi = basis_2(m).triangle(i).value;
-            r_o = basis_2(m).triangle(i).r_o;
+            vmi = basis_2(n).triangle(i).value;
+            r_o = basis_2(n).triangle(i).r_o;
 
             for k = 1:nbrIntegrationPoints
-                integral = integral + ck(k)/norm(node_1(n).coord(:)'-r_o(k,:));
+                integral = integral + ck(k)/norm(r'-r_o(k,:));
             end
 
             superBigSumX = superBigSumX+vmi(1)*integral*2*triangle_2(currentTriangle_i).air;
             superBigSumY = superBigSumY+vmi(2)*integral*2*triangle_2(currentTriangle_i).air;
             superBigSumZ = superBigSumZ+vmi(3)*integral*2*triangle_2(currentTriangle_i).air;
         end
-        tempX(m) = coef*superBigSumX;
-        tempY(m) = coef*superBigSumY;
-        tempZ(m) = coef*superBigSumZ;
+        tempX(n) = coef*superBigSumX;
+        tempY(n) = coef*superBigSumY;
+        tempZ(n) = coef*superBigSumZ;
     end
-    AX(n,:) = tempX;
-    AY(n,:) = tempY;
-    AZ(n,:) = tempZ;
+    AX(m,:) = tempX;
+    AY(m,:) = tempY;
+    AZ(m,:) = tempZ;
 end
 
 fprintf(' - Done in %5.0f sec.\n',toc);
