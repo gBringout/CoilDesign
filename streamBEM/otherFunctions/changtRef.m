9af18c161f116f65b2af6bea0c7ae16bb27f2d0e
Gael Bringout <gael.bringout@gmail.com>
2014-10-03 17:38:16 +0200

    Clarify the variables

--- streamBEM/otherFunctions/changtRef.m
+++ streamBEM/otherFunctions/changtRef.m
@@ -1,13 +1,16 @@
-function [r_o] = changtRef(A, B, C,u, v, w)
+function [P] = changtRef(P1, P2, P3,u, v, w)
 
-% This file aim at calculating the rmi for the parfor loop of Lmn9
-% That is, from the point u,v on the unitary triangle, we can calculate
-% back the coordinate (x,y,z) in the other referential
+% This file aim at calculating the position in the cartesians coordinate
+% (x,y,z) over a triangle,
+% from point in a generalized coordinate system (u,v,w). Note that w is
+% genereted using the shape function associated with a linear interpolation
+% over a unit isocele triangle.
+% The point P3 is the point with coordinate (0,0)
 
 %
 %changelog : 
 
-r_o=zeros(size(u,1),3);
+P=zeros(size(u,1),3);
 for i=1:size(u,1)
-    r_o(i,:) = B*u(i)+A*v(i)+C*w(i); % We change the coordinate from (x,y,z) to (u,v) (page 71)
+    P(i,:) = P1*u(i)+P2*v(i)+P3*w(i); % We change the coordinate from (x,y,z) to (u,v) (page 71)
 end
