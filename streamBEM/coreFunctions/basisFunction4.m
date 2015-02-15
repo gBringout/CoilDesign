a3853fd7e0c421bbb0c16d4a8abda26ab1e93257
Gael Bringout <gael.bringout@gmail.com>
2015-02-15 18:03:45 +0100

    Improve comments

--- streamBEM/coreFunctions/basisFunction4.m
+++ streamBEM/coreFunctions/basisFunction4.m
@@ -6,8 +6,6 @@ function [basis] = basisFunction4(node, triangle, center)
 % center : a vector with the coordinate of the center of the mesh
 
 
-
-
 %% Value useful for the Gauss-Legendre integration
 [u,v,ck] = triGaussPoints(2);
 for i=1:size(u,1)
@@ -21,7 +19,7 @@ for m=1:size(node,2); %For every node
         actualTriangle_i = node(m).linkToTriangle(i);
 
         % Set the 2 other nodes connected to the actual triangle,
-        % and trying to keep the order. I think it is vain because
+        % and trying to keep the order. I think that it is vain because
         % of the node reorganization made to sort the boundaries
         mNodeIndex = find(triangle(actualTriangle_i).node == m);
         aNodeIndex = mNodeIndex+1;
@@ -33,45 +31,26 @@ for m=1:size(node,2); %For every node
         Bmi = node(triangle(actualTriangle_i).node(bNodeIndex)).coord; %B - coordinate of the third node of the i_th face of the n_th node
         Cmi = node(m).coord; %C - coordinate of the first node of the i_th face of the n_th node
 
-            
-        
         norme_AC = norm(Cmi-Ami);
         norme_BC = norm(Cmi-Bmi);
         norme_AB = norm(Bmi-Ami);
-        %norme_dmi = sin(acos((norme_BC^2+norme_AC^2-norme_AB^2)/(2*norme_BC*norme_AC)))*norme_AC; % Using Al Kashi's angle and then SohCahToa stuff
-        % dmi is going from C to the middle of AB
+        
         norme_dmi = sin(acos((-norme_BC^2+norme_AC^2+norme_AB^2)/(2*norme_AB*norme_AC)))*norme_AC;% Using Al Kashi's angle and then SohCahToa stuff
 
-        
          air = (1/2)*norm(cross(Ami-Cmi,Bmi-Cmi));
-%         if air<0 % use the signed air, to determine in which direction we rotate
-%             % http://geomalgorithms.com/a01-_area.html
-%             basis(m).triangle(i).A = Bmi;
-%             basis(m).triangle(i).B = Ami;
-%             basis(m).triangle(i).C = Cmi;
-%             vmi = (Ami-Bmi)/(norme_AB*norme_dmi); %why do we divide by 2 times the air of the triangle? To match Poole definition. It comes from the normal. see peeren pdf 93
-%         else
-%             basis(m).triangle(i).A = Ami;
-%             basis(m).triangle(i).B = Bmi;
-%             basis(m).triangle(i).C = Cmi;
-%             vmi = (Bmi-Ami)/(norme_AB*norme_dmi); %why do we divide by 2 times the air of the triangle? To match Poole definition
-%         end
-%         
-        
+
         %we calculate the cross product of 2 vector of the
         %triangle, i.e. the normal of the triangle. This is a
         %vector
-        scaling = 1;%10^8;
         vectorNormal = cross(scaling*(Ami-Cmi),scaling*(Bmi-Cmi));
         % we want to compare this vector to the one going from the
         % center of the volume to on point of the surface.
         % we suppose that the center is at (0,0,0).
-        % To avoid numeric issue, we scale the vector with a factor 10^8
-        vectorOA = scaling*(Ami-center);
+        vectorOA = (Ami-center);
         % we now caclulate the dot product. If the dot product is
         % positive, the 2 vectors are going in the same direction.
         % This mean that the normal of the triangle ABC is going in
-        % the right direction:
+        % the correct direction:
         % Otherwise we have to change that.
         dotProduct = dot(vectorNormal,vectorOA);
         % if the dot product is negativ,
@@ -84,14 +63,12 @@ for m=1:size(node,2); %For every node
         % we may also do that with the normed air http://geomalgorithms.com/a01-_area.html
         if dotProduct(1) < 0
             %vector BA
-            %vmi = (Ami-Bmi)/(norme_AB*norme_dmi); %why do we divide by 2 times the air of the triangle? To match Poole definition
-            vmi = (Ami-Bmi)/(2*air); %why do we divide by 2 times the air of the triangle? To match Poole definition
+            vmi = (Ami-Bmi)/(2*air); 
             vectorNormal = cross(Bmi-Cmi,Ami-Cmi)/norm(cross((Bmi-Cmi),(Ami-Cmi)));
             basis(m).triangle(i).A = Bmi;
             basis(m).triangle(i).B = Ami;
         else
             %vector AB
-            %vmi = (Bmi-Ami)/(norme_AB*norme_dmi); %why do we divide by 2 times the air of the triangle? To match Poole definition
             vmi = (Bmi-Ami)/(2*air);
             vectorNormal = cross((Ami-Cmi),(Bmi-Cmi))/norm(cross((Ami-Cmi),(Bmi-Cmi)));
             basis(m).triangle(i).A = Ami;
@@ -104,6 +81,7 @@ for m=1:size(node,2); %For every node
         basis(m).triangle(i).air = air;
 % TODO
 % should this go in the triangle calculation???
+
         % calculate the rmi for the inductance
         % which is a referential change
         % It give us the position (x,y,z) of the integration point (u,v).
--- streamBEM/otherFunctions/changtRef.m
+++ streamBEM/otherFunctions/changtRef.m
@@ -1,16 +1,11 @@
 function [P] = changtRef(P1, P2, P3,u, v, w)
+% This file aim at calculating the changement of coordinates for a triangle.
 
-% This file aim at calculating the position in the cartesians coordinate
-% (x,y,z) over a triangle,
-% from point in a generalized coordinate system (u,v,w). Note that w is
-% genereted using the shape function associated with a linear interpolation
-% over a unit isocele triangle.
-% The point P3 is the point with coordinate (0,0)
-
-%
-%changelog : 
+% u,v,w: coordinate on the unit triangle
+% P1, P2, P3: coordinate in the global coordinate system. Note that the
+% point P3 is the one maped with the point (0,0)
 
 P=zeros(size(u,1),3);
 for i=1:size(u,1)
-    P(i,:) = P1*u(i)+P2*v(i)+P3*w(i); % We change the coordinate from (x,y,z) to (u,v) (page 71)
+    P(i,:) = P1*u(i)+P2*v(i)+P3*w(i);
 end
